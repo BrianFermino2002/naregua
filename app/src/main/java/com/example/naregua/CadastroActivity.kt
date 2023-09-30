7ee398c8f82ea6fcd6763214575dd378b13a1275
BrianFermino2002 <brianirado@gmail.com>
2023-09-30 15:31:52 -0300

    Segundo Commit

--- app/src/main/java/com/example/naregua/CadastroActivity.kt
+++ app/src/main/java/com/example/naregua/CadastroActivity.kt
@@ -1,14 +1,11 @@
-package com.example.naregua;
+package com.example.naregua
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-import android.os.Bundle;
-
-public class Tela_de_cadastro extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tela_de_cadastro);
+class CadastroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_de_cadastro)
     }
 }
