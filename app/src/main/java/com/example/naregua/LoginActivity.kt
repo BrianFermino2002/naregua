fc9bf5611f1afadaa2025516056abad6534e1135
BrianFermino2002 <brianirado@gmail.com>
2023-10-04 23:43:17 -0300

    Alterações na tela de cadastro

--- app/src/main/java/com/example/naregua/LoginActivity.kt
+++ app/src/main/java/com/example/naregua/LoginActivity.kt
@@ -1,14 +1,11 @@
-package com.example.naregua;
+package com.example.naregua
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-import android.os.Bundle;
-
-public class Tela_de_login extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tela_de_login);
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_de_login)
     }
 }
