ae785aa83a7174b845f51eb5ff8aecbe649be9e7
BrianFermino2002 <brianirado@gmail.com>
2023-10-15 23:37:57 -0300

    Adição da tela principal e da tela pesquisa

---
+++ app/src/main/java/com/example/naregua/FragmentHomeUser.kt
@@ -0,0 +1,20 @@
+package com.example.naregua
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.naregua.databinding.FragmentHomeUserBinding
+
+class FragmentHomeUser: Fragment() {
+    private lateinit var binding: FragmentHomeUserBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return binding.root
+    }
+}
