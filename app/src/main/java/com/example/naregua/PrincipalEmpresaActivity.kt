d94dfe8fe1752dff7addc3dc01b6c9fb6cbbc17b
BrianFermino2002 <brianirado@gmail.com>
2023-10-24 23:33:19 -0300

    Adição da tela Principal da Empresa

---
+++ app/src/main/java/com/example/naregua/PrincipalEmpresaActivity.kt
@@ -0,0 +1,11 @@
+package com.example.naregua
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PrincipalEmpresaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_principal_empresa)
+    }
+}
