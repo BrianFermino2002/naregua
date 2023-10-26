ab96e0bd59657558478e6fe7645cd6d91d4d8297
BrianFermino2002 <brianirado@gmail.com>
2023-10-26 17:18:45 -0300

    Lógicas das telas Main e Cadastro

--- app/src/main/java/com/example/naregua/CadastroActivity.kt
+++ app/src/main/java/com/example/naregua/CadastroActivity.kt
@@ -1,11 +1,37 @@
 package com.example.naregua
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.naregua.databinding.ActivityTelaDeCadastroBinding
 
 class CadastroActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTelaDeCadastroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tela_de_cadastro)
+        setContentView(binding.root)
+
+        binding.ibVoltar.setOnClickListener {
+            finish()
+        }
+        binding.button2.setOnClickListener {
+            if(verificaDados()){
+
+            }else{
+                Toast.makeText(this, "Ainda faltam campos a serem preenchidos", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    fun verificaDados(): Boolean{
+        return !(binding.editEmail.text.toString().isEmpty() ||
+                binding.editTextText.text.toString().isEmpty() ||
+                binding.textView3.text.toString().isEmpty() ||
+                binding.editTextNumber.text.toString().isEmpty() ||
+                binding.editTextNumber2.text.toString().isEmpty() ||
+                binding.editTextNumber3.text.toString().isEmpty() ||
+                binding.editTextNumber4.text.toString().isEmpty() ||
+                binding.editTextNumber5.text.toString().isEmpty() ||
+                binding.editTextTextPassword.text.toString().isEmpty())
     }
 }
--- app/src/main/java/com/example/naregua/MainActivity.kt
+++ app/src/main/java/com/example/naregua/MainActivity.kt
@@ -1,13 +1,30 @@
 package com.example.naregua
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.naregua.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
+        binding.btnSoucadastrado.setOnClickListener {
+            val intent =  Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnSemcadastro.setOnClickListener {
+            val intent = Intent(this, PrincipalUserActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnCadastrar.setOnClickListener {
+            val intent = Intent(this, CadastroActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
